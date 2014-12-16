91d210c392dfbd46a1b0332384de7e3fcf11f215
r04r <r04r@minichan.org>
2014-12-16 18:45:27 +0100

    Removem entions of minichan from stuff.php

--- stuff.php
+++ stuff.php
@@ -29,10 +29,9 @@ $page_title = 'Stuff';
 	</ul>
 	<ul class="stuff">
 		<li><strong><a href="<?php echo DOMAIN; ?>chat">Chat (IRC)</a></strong> — <span class="unimportant">Have some biscuits and tea.</span></li>
-		<li><a href="http://status.minichan.org">Blog</a> — <span class="unimportant">Off-site news and updates.</span></li>
 <!--		<li><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&amp;business=7P4QPNBZJ4R7N&amp;lc=GB&amp;item_name=TinyBBS&amp;item_number=TinyBBS%20Donation&amp;currency_code=USD&amp;bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted" alt="Donate" target="_new">Donate</a> — <span class="unimportant">Contribute towards the site costs.</span></li> -->
 		<li><a href="<?php echo DOMAIN; ?>compose_message/mods">Contact</a> — <span class="unimportant">Send a personal message to the mod(s).</span></li>
-		<li><strong><a href="<?php echo DOMAIN; ?>addons">Add-ons</a></strong> — <span class="unimportant">Minichan add-ons, ready to take over the world.</span></li>
+		<li><strong><a href="<?php echo DOMAIN; ?>addons">Add-ons</a></strong> — <span class="unimportant">Add-ons, ready to take over the world.</span></li>
 		<li><strong><a href="<?php echo DOMAIN; ?>link">MiniURL</a></strong> — <span class="unimportant">Minify links</span></li>
 		<?php
 		if(MOBILE_MODE){
