1413adfda08617bf2bb4fdc864b628cefb9121b5
r04r <r04r@minichan.org>
2015-08-28 12:30:33 +0200

    fix(?) mobile mode when there are long words in headlines

--- includes/template.php
+++ includes/template.php
@@ -33,7 +33,7 @@
 		<meta name="keywords" content="minichan, bbs, board, anonymous, free, debate, discuss, argue, drama, loldrama, youarenowbrowsingmanually" />
 		<title><?php echo strip_tags($page_title) . ' — ' . SITE_TITLE ?></title>
 		<link rel="icon" type="image/gif" href="<?php echo STATIC_DOMAIN; ?>favicon.gif" />
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo (STATIC_DOMAIN . 'style/layout.css') ?>?9" />
+		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo (STATIC_DOMAIN . 'style/layout.css') ?>?10" />
 		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo (($custom_stylesheet) ? htmlspecialchars($custom_stylesheet) : (STATIC_DOMAIN . 'style/' . $stylesheet . '.css?2')) ?>" />
 		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo (STATIC_DOMAIN . 'javascript/highlight-styles/vs.css') ?>" />
 		<?php if(MOBILE_MODE){ ?>
