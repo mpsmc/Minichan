11e3522cf6c856cb62c79f57390bd7431b2abf3a
r04r <r04r@minichan.org>
2015-12-15 22:29:56 +0100

    Fix shuffle

--- shuffle.php
+++ shuffle.php
@@ -2,7 +2,7 @@
 require 'includes/header.php';
 force_id();
 update_activity('shuffle', 1);
-$stmt = $link->db_exec('SELECT id FROM topics WHERE deleted = 0 AND locked = 0 AND ((UNIX_TIMESTAMP()-last_post)<%1) ORDER BY RAND() LIMIT 1', NECRO_BUMP_TIME);
+$stmt = $link->db_exec('SELECT id FROM topics WHERE deleted = 0 AND stealth_ban = 0 AND locked = 0 AND ((UNIX_TIMESTAMP()-last_post)<%1) ORDER BY RAND() LIMIT 1', NECRO_BUMP_TIME);
 list($shuffle_topic_id) = $link->fetch_row($stmt);
 redirect('There you go — a semi-random topic!', 'topic/'.$shuffle_topic_id);
 require 'includes/footer.php';
